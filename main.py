732607c206a6dd1f19d04dc877c50b0093859c81
chaoukiiiii <chaoukitrabelsi279@gmail.com>
2022-03-31 12:48:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import os
-import re
-
 from flask import Flask, request
 from google.cloud import bigquery
 from google.cloud import storage
-
+from google.api_core.exceptions import NotFound
 
 app = Flask(__name__)
 
@@ -20,6 +18,7 @@ def entry():
     dataset=os.environ.get('DATASET')
     table_name=os.environ.get('TABLENAME')
     archive_folder=os.environ.get('ARCHIVEFOLDER')
+    ################### values example for envirement variable #################
     #bucket = "gs://cloud-run-bq-celine"
     #folder = "covid_folder"
     #pattern="example_data_covid"
@@ -35,19 +34,41 @@ def entry():
     print("dataset Name :", dataset)
     print("table Name :",table_name)
     print("archive_folder Name :",archive_folder)
+    ########## test if the envirement variables are set correctly: ##########
+    if bucket is None or not(bucket.startswith("gs://")):
+        print("Error: bucket environment variable is not defined correctly")
+        return ("Error bucket environment variable is not defined correctly.", 500)
+    if table_name is None or folder is None or pattern is None or delimiter is None or dataset is None or archive_folder is None:
+        print("Error:  environments variables are not defined correctly")
+        return ("Error  environments variables are not defined correctly.", 500)
+    if (len(delimiter)!=1):
+        print("Error: Delimiter environment variable is not defined correctly")
+        return ("Error: Delimiter environment variable is not defined correctly.", 500)
+
+    #set destination file + uri of csv files
     table=dataset+"."+table_name
-    # If the user has not set the environment variable for the table then error out
-    #if table is None:
-    #    print("Error: Table env variable not set, so returning.")
-    #    return ("Error BIGQUERY_TABLE environment variable is not set.", 500)
     uri=bucket+"/"+folder+"/"+pattern+"*.csv"
+    # get files from uri
+    storage_client = storage.Client()
+    
+    bucket_initial = storage_client.get_bucket(bucket)
+    blobs = bucket_initial.list_blobs(prefix=folder+'/'+pattern)
+    if len(blobs)==0:
+        print("Warning:  there is No files match the provided pattern")
+        return ("Warning  there is No files match the provided pattern.", 200)
+
+    try:
+       client.get_dataset(dataset)  # Make an API request.
+       print("Dataset {} already exists".format(dataset_id))
+    except NotFound:
+        print("Dataset {} is not found".format(dataset_id))
+        return ("Error  there is No Dataset matchs the provided dataset variable.", 500)
     # Setup the job to append to the table if it already exists and to autodetect the schema
     job_config = bigquery.LoadJobConfig(
-    autodetect=True,
-    skip_leading_rows=1,
     write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
-    source_format=bigquery.SourceFormat.CSV
-    
+    source_format=bigquery.SourceFormat.CSV,
+    skip_leading_rows=1,
+    autodetect=True
     )
 
     # Run the load job
@@ -56,15 +77,11 @@ def entry():
     # Run the job synchronously and wait for it to complete
     load_job.result()
 
-    print ("Loaded file located at")
-    storage_client = storage.Client()
-    #bucket="gs://cloud-run-bq-celine"
-    bucket=bucket[5:]
-    bucket_initial = storage_client.get_bucket(bucket)
-    blobs = bucket_initial.list_blobs(prefix=folder+'/'+pattern)
+    print ("Loaded files located at ",bucket, " /",folder,"/")
+    
     for i in blobs:
-        bucket_initial.rename_blob(i, new_name=i.name.replace(folder+'/', archive_folder+'/archived'))
+        bucket_initial.rename_blob(i, new_name=i.name.replace(folder+'/', archive_folder+'/archived_'))
 
-
+    print ("Archives files to ",bucket, " /",archive_folder,"/")
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
